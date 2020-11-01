18152d79db87c20f5f2f75d620439adcaf6a3e3e
raduwolf <e@yy.com>
2020-11-01 20:03:02 +0200

    some classes

--- src/java/main/org/sci/temaoop/abstractclasses/Car.java
+++ src/java/main/org/sci/temaoop/abstractclasses/Car.java
@@ -1,4 +1,61 @@
 package org.sci.temaoop.abstractclasses;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public abstract class Car {
+
+    private String licensePlate;
+    private String manufacturer;
+    private String model;
+    private int yearOfManufacture;
+
+
+    public abstract List<String> getDefects();
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYearOfManufacture() {
+        return yearOfManufacture;
+    }
+
+    public void setYearOfManufacture(int yearOfManufacture) {
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return yearOfManufacture == car.yearOfManufacture &&
+                Objects.equals(licensePlate, car.licensePlate) &&
+                Objects.equals(manufacturer, car.manufacturer) &&
+                Objects.equals(model, car.model);
+    }
+
+
 }
---
+++ src/java/main/org/sci/temaoop/models/SUV.java
@@ -0,0 +1,12 @@
+package org.sci.temaoop.models;
+
+import org.sci.temaoop.abstractclasses.Car;
+
+import java.util.List;
+
+public class SUV extends Car {
+    @Override
+    public List<String> getDefects() {
+        return null;
+    }
+}
